5fdb08ba582f45890b4cc49cab94b7b3a0d4f49e
Theodor Dumitru <teddy.dumitru@yahoo.com>
2022-10-21 15:56:35 -0400

    updated.

--- login/admin.py
+++ login/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import UsersCreationForm, UsersChangeForm
-from .models import Users
+from .models import Users, FriendProfile, FriendRequest
 # Register your models here.
 
 class UsersAdmin(UserAdmin):
@@ -11,4 +11,23 @@ class UsersAdmin(UserAdmin):
     model = Users
     list_display = ["email", "username"]
 
+class FriendRequestAdmin(admin.ModelAdmin):
+    model = FriendRequest
+
+    list_display = ["to_request", "from_request"]
+
+class FriendsInline(admin.TabularInline):
+    model = FriendProfile.friends.through
+
+class FriendProfileAdmin(admin.ModelAdmin):
+    model = FriendProfile
+
+    inlines = [
+        FriendsInline
+    ]
+
+    list_display = ["user", "get_friend_request"]
+
 admin.site.register(Users, UsersAdmin)
+admin.site.register(FriendRequest, FriendRequestAdmin)
+admin.site.register(FriendProfile, FriendProfileAdmin)
--- login/models.py
+++ login/models.py
@@ -5,3 +5,18 @@ from django.contrib.auth.models import AbstractUser
 class Users(AbstractUser):
     def __str__(self):
         return self.username
+
+class FriendRequest(models.Model):
+    to_request = models.OneToOneField(Users, related_name='to_request', on_delete=models.CASCADE)
+    from_request = models.OneToOneField(Users, related_name='from_request', on_delete=models.CASCADE)
+
+class FriendProfile(models.Model):
+    user = models.OneToOneField(Users, related_name='user', on_delete=models.CASCADE)
+    friends = models.ManyToManyField(Users, blank=True)
+    friend_requests = models.ForeignKey(FriendRequest, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_friend_request(self):
+        return (self.friend_requests.to_request.username, self.friend_requests.from_request.username)
