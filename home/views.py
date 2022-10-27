dec7b2f56232836354f4f9e4aacd3d764f52c1f9
Theodor Dumitru <teddy.dumitru@yahoo.com>
2022-10-26 20:34:36 -0400

    finished with my friend system

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
@@ -26,10 +27,12 @@ def home_view(request):
         context['in_friends'] = False
         return render(request, 'home_template.html', context)
     else:
+        if request.user.is_authenticated:
+            context["FriendProfile"] = FriendProfile.objects.get(user=request.user)
+        context['in_friends'] = True
         if request.POST.get('accept'):
             #adds a friend to two users friend profiles and deletes the friend request
             #and tells the template that the user is accessing the friends interface
-            context['in_friends'] = True
             accepting = FriendRequest.objects.get(to_request=request.user, from_request=Users.objects.get(username=request.POST.get('accept')))
 
             profile = FriendProfile.objects.get(user=request.user)
@@ -43,16 +46,51 @@ def home_view(request):
             accepting.delete()
             context['in_friends'] = 'initial'
         elif request.POST.get('cancel'):
-            context['in_friends'] = True
-            #deletes a friend request
+            #cancels a friend request
             canceling = FriendRequest.objects.get(to_request=Users.objects.get(username=request.POST.get('cancel')), from_request=request.user)
             canceling.delete()
         elif request.POST.get('send_request'):
-            context['in_friends'] = True
             try:
-                user = Users.objects.get(username=request.POST.get('send_request'))
+                #gets the users that are related to the friend request
+                #creates the friend request object
+                #adds the object to the user's friend profiles
+                to_user = Users.objects.get(username=request.POST.get('send_request'))
+                from_user = request.user
+                to_profile = FriendProfile.objects.get(user=to_user)
+                from_profile = FriendProfile.objects.get(user=from_user)
+
+                #checks if the profile already has the user as a friend or
+                #sent them a friend request
+                already_friends = from_profile.friends.get_queryset().filter(username=to_user.username)
+                if already_friends.count() != 0:
+                    context['already_friends'] = True
+                    return render(request, 'home_template.html', context)
+
+                already_sent_request = from_profile.friend_requests.get_queryset().filter(from_request=from_user, to_request=to_user)
+                if already_sent_request.count() != 0:
+                    context['already_sent_request'] = True
+                    return render(request, 'home_template.html', context)
+
+                friend_request = FriendRequest.objects.create(to_request=to_user, from_request=from_user)
+                
+                to_profile.friend_requests.add(friend_request)
+                from_profile.friend_requests.add(friend_request)
+
+                friend_request.save()
+                to_profile.save()
+                from_profile.save()
             except ObjectDoesNotExist:
                 context["cant_find_user"] = True
-        if request.user.is_authenticated:
-            context["FriendProfile"] = FriendProfile.objects.get(user=request.user)
+        elif request.POST.get('delete'):
+            #deletes a friend from a user's friend profile
+            del_user = Users.objects.get(username=request.POST.get('delete'))
+            curr_user = request.user
+            del_user_profile = FriendProfile.objects.get(user=del_user)
+            curr_user_profile = FriendProfile.objects.get(user=curr_user)
+
+            del_user_profile.friends.remove(curr_user)
+            curr_user_profile.friends.remove(del_user)
+
+            del_user_profile.save()
+            curr_user_profile.save()
         return render(request, 'home_template.html', context)
